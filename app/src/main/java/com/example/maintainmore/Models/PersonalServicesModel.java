da012ce12a6b694f96ca18791c1f79dc4d32d263
Suraj Devraye <surajdevraye@gmail.com>
2022-01-22 14:57:16 +0530

    advance item click

---
+++ app/src/main/java/com/example/maintainmore/Models/PersonalServicesModel.java
@@ -0,0 +1,26 @@
+package com.example.maintainmore.Models;
+
+public class PersonalServicesModel {
+    String name, image;
+
+    public PersonalServicesModel(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
